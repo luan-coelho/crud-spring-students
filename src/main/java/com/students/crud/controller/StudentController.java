ac724e9589b8dc335a2a5bbf1c97842e29f7bfbd
luan-coelho <luancoelho.dev@gmail.com>
2022-03-15 20:50:10 -0300

    View implementation

--- src/main/java/com/students/crud/controller/StudentController.java
+++ src/main/java/com/students/crud/controller/StudentController.java
@@ -1,18 +1,18 @@
 package com.students.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.students.crud.model.Student;
 import com.students.crud.service.StudentService;
 
-@RestController
+@Controller
 @RequestMapping("/students")
 public class StudentController {
 
@@ -23,7 +23,7 @@ public class StudentController {
 	public String findAll(Model model) {
 		model.addAttribute("students", studentService.findAll());
 		
-		return "students";
+		return "students.html";
 	}
 	
 	@GetMapping("/register")
--- src/main/java/com/students/crud/repository/StudentRepository.java
+++ src/main/java/com/students/crud/repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.students.crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.students.crud.model.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	
 }
